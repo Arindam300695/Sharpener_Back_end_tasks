77d2008c6e5452a58f64fc76e5926de9618a5168
Arindam Chattopadhyay <arindam.chattopadhyay300695@gmail.com>
2023-04-16 22:03:01 +0530

    completed sending routing based responses

--- kickstarting_node_js/server.js
+++ kickstarting_node_js/server.js
@@ -1,7 +1,11 @@
 const http = require("http");
 const PORT = 4000;
 const server = http.createServer((req, res) => {
-  res.write("Arindam Chattopadhyay"); //write a response to the client
+  const url = req.url;
+  if (url === "/home") res.write("Welcome home");
+  else if (url === "/about") res.write("Welcome to About Us page");
+  else if (url === "/node") res.write("Welcome to my Node Js project");
+  else res.write("Welcome to my world!"); //write a response to the client
   res.end(); //end the response
 });
 server.listen(PORT, (error) => {
