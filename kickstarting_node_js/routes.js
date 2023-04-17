76d1de706d1b08f1c591c18a2279a6a0928e98ec
Arindam Chattopadhyay <arindam.chattopadhyay300695@gmail.com>
2023-04-17 21:52:09 +0530

    all error tasks done of the first module

--- kickstarting_node_js/routes.js
+++ kickstarting_node_js/routes.js
@@ -2,6 +2,22 @@ const fs = require("fs");
 const routeHandler = (req, res) => {
   const url = req.url;
   const method = req.method;
+  if (url == "/message" && method == "POST") {
+    res.setHeader("Content-Type", "text/html");
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[0];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    res.end();
+  
+
   if (url === "/") {
     const fileData = fs.readFileSync("./message.txt", "utf8");
     res.write("<html>");
@@ -13,20 +29,10 @@ const routeHandler = (req, res) => {
     res.write("</html>");
     return res.end();
   }
-  if (url == "/message" && method == "POST") {
-    res.setHeader("Content-Type", "text/html");
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFileSync("message.txt", message);
-    });
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server</h1></body>");
+  res.write("</html>");
 };
 module.exports = routeHandler;
