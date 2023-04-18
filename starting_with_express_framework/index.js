45ef53962fa98983b129d4e695d35ef51d3970ea
Arindam Chattopadhyay <arindam.chattopadhyay300695@gmail.com>
2023-04-18 09:41:46 +0530

    task 2 of second module is done

--- starting_with_express_framework/index.js
+++ starting_with_express_framework/index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
 const PORT = 4000;
-app.use((req, res, next) => {
-  console.log("in the middleware");
-  next();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='message'/>Product Name  <input type='number' name='size'/>Product Size<button type='submit'>Send</button></form>"
+  );
 });
-app.use((req, res, next) => {
-  console.log("in another middleware");
+app.use("/product", (req, res, next) => {
+  console.log(
+    "product name is:",
+    req.body.message,
+    "and size is:",
+    req.body.size
+  );
+  res.redirect("/");
+});
+app.use("/", (req, res, next) => {
   res.send("<h1>Welcome to Express!</h1>");
 });
 
